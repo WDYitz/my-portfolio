a7562cde05cbfa8565541edb2c2e674f8399d959
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-12-04 01:45:59 -0300

    refactor: Add RootLayout component for internationalization support

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,32 @@
+import Header from "@/components/header";
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ weight: ["400", "500", "600", "700"], subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "Yitzhak B. Rodriguez",
+    template: '%ns | Yitzhak B. Rodriguez',
+  },
+  description: "Hi im Yitzhak B. Rodriguez Fullstack Developer.",
+};
+
+interface RootLayoutProps {
+  children: Readonly<React.ReactNode>;
+}
+
+export default function RootLayout({
+  children,
+}: RootLayoutProps) {
+  return (
+
+    <html lang="en">
+      <body className={`${poppins} antialiased dark`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
