ca9b137f3c165ddf237ce5d3945a4ed2e20a0a2e
Yitzhak B. Rodriguez <112505107+WDYitz@users.noreply.github.com>
2025-04-27 21:54:53 -0300

    Revert "Revert "Dev/downgrade""

--- src/app/[locale]/(main)/_components/fire-emoji-animation.tsx
+++ src/app/[locale]/(main)/_components/fire-emoji-animation.tsx
@@ -1,13 +1,18 @@
+import { cn } from "@/lib/utils"
 import Image from "next/image"
 
-const FireEmojiAnimation = () => {
+type FireEmojiAnimationProps = {
+  className?: string
+}
+
+const FireEmojiAnimation = ({ className }: FireEmojiAnimationProps) => {
   return <Image
     priority
     src="/gifs/fire-animation-2.gif"
     alt="background animation"
     width={500}
     height={500}
-    className="z-[-100] absolute top-0 left-0 w-full h-[200px] md:w-[80%] md:h-[400px]"
+    className={cn("z-[-100] absolute w-full h-[200px] md:w-[80%] md:h-[400px]", className)}
     unoptimized
   />
 }
